c0ea20a0adf8605141b843ad4a8c0fa06d630cfe
Aleksandra Volchikhina <aleksandravolcihina@gmail.com>
2026-05-19 19:35:28 +0300

    4st commit

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,4 +1,34 @@
 from django.test import TestCase
+from catalog.models import Category, Product
 
-# Create your tests here.
-# Тесты будут добавлены позже
+
+class CategoryModelTest(TestCase):
+    """Тесты для модели Category"""
+
+    def test_category_creation(self):
+        category = Category.objects.create(
+            name='Тестовая категория',
+            description='Описание тестовой категории'
+        )
+        self.assertEqual(str(category), 'Тестовая категория')
+        self.assertEqual(category.name, 'Тестовая категория')
+
+
+class ProductModelTest(TestCase):
+    """Тесты для модели Product"""
+
+    def setUp(self):
+        self.category = Category.objects.create(
+            name='Тестовая категория',
+            description='Описание тестовой категории'
+        )
+
+    def test_product_creation(self):
+        product = Product.objects.create(
+            name='Тестовый продукт',
+            description='Описание тестового продукта',
+            price=99.99,
+            category=self.category
+        )
+        self.assertEqual(str(product), 'Тестовый продукт ($99.99)')
+        self.assertEqual(product.price, 99.99)
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Подключаем URL-адреса приложения catalog
-    path('', include('catalog.urls')),  # Все URL заканчиваются на /
+    path('', include('catalog.urls')),
 ]
+
+# Добавляем поддержку медиафайлов в режиме отладки
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
